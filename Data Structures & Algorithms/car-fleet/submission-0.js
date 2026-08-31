b13a3b2fec177a5ffa8810d36930c6ac39183fe2
Kamrul Hassan Anik <89210298+kh-anik26@users.noreply.github.com>
2026-08-31 22:39:50 +0600

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const pair = position.map((p, i) => [p, speed[i]]);
+        
+        // Sort in descending order based on position (reverse sorted order)
+        pair.sort((a, b) => b[0] - a[0]);
+
+        const stack = [];
+
+        for (const [p, s] of pair) {
+            stack.push((target - p) / s);
+
+            if (
+                stack.length >= 2 &&
+                stack[stack.length - 1] <= stack[stack.length - 2]
+            ) {
+                stack.pop();
+            }
+        }
+
+        return stack.length;
+    }
+}
