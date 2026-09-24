2fdd54834ec72eb01db3707849ea9e65509c1d46
Kamrul Hassan Anik <89210298+kh-anik26@users.noreply.github.com>
2026-09-24 18:08:01 +0600

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        nums.sort((a,b) => a -b);
+        for (let i = 0; i < nums.length; i++){
+            if(nums[i] === nums[i + 1]){
+                return nums[i];
+            }
+        }
+    }
+}
